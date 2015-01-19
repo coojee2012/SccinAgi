23e83880d6b909f28515255f39a59b710525758b
coojee2012 <11366846@qq.com>
2015-01-19 17:29:53 +0800

    添加easyUI

--- ReportAPI/App/Server.js
+++ ReportAPI/App/Server.js
@@ -1,57 +1,99 @@
 /**
  * Created by LinYong on 2015-01-16.
  */
-var fs = require('fs'),
-    path = require('path'),
-    util = require('util'),
-    http = require('http'),
-    domain = require('domain'),
-    express = require('express'),
+var httpServer=(function() {
+    var fs = require('fs'),
+        path = require('path'),
+        util = require('util'),
+        http = require('http'),
+        domain = require('domain'),
+        express = require('express'),
 
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    session = require('express-session'),
-    serverDm = domain.create(),
+        bodyParser = require('body-parser'),
+        methodOverride = require('method-override'),
+        session = require('express-session'),
+        serverDm = domain.create(),
 
-    processPath = path.dirname(process.argv[1]);
+        processPath = path.dirname(process.argv[1]);
 
 
-serverDm.on('error', function (error) {
-    delete error.domain;
-    log("应用程序发生异常："+error);
-});
-
-var server=http.createServer();
-server.on('request', handler);
-server.maxHeadersCount = 0;
-server.on('error', function (error) {
-    log('WEB服务发生严重错误: '+ error);
-});
-server.on('close', function () {
-    log('WEB服务关闭! ');
-});
-
-var domainRun={};
-
-domainRun.start=function(){
-    serverDm.run(function(){
-        server.listen(80,function(){
-           log('REST API启动成功！ ');
-        });
+    serverDm.on('error', function (error) {
+        delete error.domain;
+        log("应用程序发生异常：" + error);
     });
-}
-domainRun.stop=function(){
-    server.close();
-    serverDm.exit();
-}
 
-function handler(req,res){
+    var server = http.createServer();
+    server.on('request', handler);
+    server.maxHeadersCount = 0;
+    server.timeout=10000;//3秒
+    server.on('error', function (error) {
+        log('REST API服务发生严重错误: ' + error);
+    });
+    server.on('close', function () {
+        log('REST API服务关闭! ');
+         serverDm.exit();
+    });
+    server.on('timeout',function(socket){
+        socket.destroy();
+        log('Connection TimeOut!');
+    });
 
-}
+    var app=express();
+    app.set('port', 18081 ||  process.env.PORT);
+    //app.set('views', path.join(__dirname, 'app/dist'));
+    //app.set('view engine', 'ejs');
+    //app.set('env', "development" || YuanCRM.conf.appConf.env);
+   // app.engine('html', require('ejs').renderFile);
+    //app.use(bodyParser());
+    app.use(session({
+        secret: 'say web cat!'
+    }));
 
-function log(str){
-    str=">"+str+"</br>";
-    window.$("#log").append(str);
-}
-exports.domainRun=domainRun;
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+
+    app.use(methodOverride());
+
+    app.use(function (req, res, next) {
+
+        res.send("aaaaa");
+    });
+    function Server(){
+        this.connects=0;
+    };
+    Server.prototype.start = function (callback) {
+        var self=this;
+        serverDm.run(function () {
+            server.listen(app.get('port'), function (err) {
+                if(err){
+                    log('REST API启动失败！原因： '+err);
+                }else{
+                    log('REST API启动成功！监听端口： '+app.get('port'));
+                    if(typeof(callback)==='function') {
+                        callback();
+                    }
+
+                }
+
+
+            });
+        });
+    }
+    Server.prototype.stop = function (callback) {
+        server.close(callback);
+    }
+
+    function handler(req, res) {
+        app(req,res);
+    }
+
+    function log(str) {
+        str = ">" + str + "\r\n</br>";
+        window.$("#log").append(str);
+    }
+    return new Server();
+
+})();
 
