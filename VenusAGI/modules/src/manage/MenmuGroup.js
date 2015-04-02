1f62c7cd254c82ced00192cabf85152374f75ed9
coojee2012 <11366846@qq.com>
2015-04-02 21:10:31 +0800

    1

---
+++ VenusAGI/modules/src/manage/MenmuGroup.js
@@ -0,0 +1,15 @@
+var manageMenmuGroup=schema.define('manageMenmuGroup',{
+    id:{type:String,length:100,default:function(){return guid.create();}},
+    groupName:   {type:String,length:50},
+    crtTime:   {type: String,length:50,index: true, default: function () { return moment().format("YYYY-MM-DD HH:mm:ss"); }}
+});
+
+
+
+
+manageMenmuGroup.validatesPresenceOf('groupName');//验证非空
+
+manageMenmuGroup.Name='manageMenmuGroup';
+schema.models.manageMenmuGroup;
+exports.manageMenmuGroup = manageMenmuGroup;
+Dbs.manageMenmuGroup = manageMenmuGroup;
