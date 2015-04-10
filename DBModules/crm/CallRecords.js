6b715cb992c726e885a8bb6d49f8c64c4f9a8ce1
coojee2012 <11366846@qq.com>
2015-04-10 21:18:55 +0800

    修改表结构

--- DBModules/crm/CallRecords.js
+++ DBModules/crm/CallRecords.js
@@ -3,7 +3,7 @@ var crmCallRecords = schema.define('crmCallRecords', {
     customId: {type: String, length: 50},//客户编号
     userId: {type: Number, default: 0},//用户编号
     poptype:{type: String, length: 10,default:'呼入'},//呼叫方向
-    callNumber:{type: Number, default:''},
+    callNumber:{type: String, default:''},
     recordType:{type: String, length: 10},
     record:{type: String, length: 500},
     createTime: {
